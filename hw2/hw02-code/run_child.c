2d1cd502aefc4157f0b41c9dd247ab637fb575d4
Alex Biedny <abiedny09@gmail.com>
2020-02-03 15:34:04 -0600

    Some stuff

---
+++ hw2/hw02-code/run_child.c
@@ -0,0 +1,16 @@
+// run_child.c: run a child process. This version needs fixin'.
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main(void){
+  char *child_argv[] = {"ls","-l","-ah",NULL};
+  char *child_cmd = "ls";
+  printf("Running command '%s'\n",child_cmd);
+  printf("------------------\n");
+  execvp(child_cmd,child_argv);
+  printf("------------------\n");
+  printf("Child Finished\n");         // show that the child process has finished
+  return 0;
+}
